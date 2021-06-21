08670d91322cde43cc73b450798a8458010d2c1d
Andreas Hellander <andreas.hellander@gmail.com>
2021-06-21 12:57:33 +0200

    Fist commit train.py and init_model.py

--- client/train.py
+++ client/train.py
@@ -1,20 +1,43 @@
-import keras
-import pickle
-import numpy as np
 import os
+from __future__ import print_function
+import sys
+import tensorflow as tf
+import tensorflow.keras as keras 
+import tensorflow.keras.models as krm
+import pickle
+import yaml
 
-from AMLmodel import ML_model
+from models.AMLmodel import AMLModel
+from data.load_data import load_processed
+from data.read_data import read_data
 
-from src.data.load_data import load_processed
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 
-model = ML_model()
+def train(model, settings):
+    print("-- RUNNING TRAINING --", flush=True)
 
-(x_train, y_train, x_val, y_val) = load_processed('data.p')
+    (x_train, y_train, x_val, y_val) = load_processed('data.p')
+    model.fit(x_train, y_train, x_val, y_val, batch_size=8, data_augmentation=False)
 
-model.fit(x_train, y_train, x_val, y_val, batch_size=8, data_augmentation=False)
-model_json = model.model.to_json()
-f = open('model.json', 'w')
-f.write(model_json)
-f.close()
-model.model.save_weights('model_weights.h5')
+    print("-- TRAINING COMPLETED --", flush=True)
+    return model
+
+if __name__ == '__main__':
+
+    with open('settings.yaml', 'r') as fh:
+        try:
+            settings = dict(yaml.safe_load(fh))
+        except yaml.YAMLError as e:
+            raise(e)
+
+    from fedn.utils.kerashelper import KerasHelper
+    helper = KerasHelper()
+    weights = helper.load_model(sys.argv[1])
+
+    from models.AMLmodel import construct_model
+    model = construct_model()
+    model.set_weights(weights)
+    
+    model = train(model,settings)
+    helper.save_model(model.get_weights(),sys.argv[2])
