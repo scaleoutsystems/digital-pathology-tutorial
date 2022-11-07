311675b938df05a6448d32d25e631605088a42b4
Andreas Hellander <andreas.hellander@it.uu.se>
2022-11-07 22:39:18 +0100

    Update prepare_dataset.py

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -8,8 +8,7 @@ if __name__ == '__main__':
 
     nr_of_partitions = int(sys.argv[1])
 
-    #amldata = r'dataset/raw/AML-Cytomorphology/'
-    amldata = r'/home/jovyan/work/minio-vol/data/AML-Cytomorphology'
+    amldata = r'dataset/raw/AML-Cytomorphology/'
     classes = {}
     class_index = 0
     class_sizes = {}
